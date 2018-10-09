152490250614088688373ec4b93452d26bd24f77
ASavrova <43751079+ASavrova@users.noreply.github.com>
2018-10-09 23:35:52 +0700

    Add files via upload

---
+++ Savrova/3.0/3.0.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int sort (int *arr, int length)
+{
+    int temp, index1, index2;
+    for (index1 = 0; index1 < length ; index1++)
+        for ( index2 = 0; index2 < length - index1 - 1; index2++)
+        if (arr[index2] > arr[index2 + 1])
+        {
+            temp = arr[index2];
+            arr[index2] = arr[index2 + 1];
+            arr[index2 +1] = temp;
+        }
+        return *arr;
+}
+int main()
+{
+    int length;
+    int *arr;
+    cin >> length;
+    arr = (int*)malloc(length * sizeof(int));
+    for (int index = 0; index < length; index++)
+    cin >> arr[index];
+    sort (arr, length);
+	for (int index = 0; index < length; index++)
+    cout << arr[index]<< ' ';
+return 0;
+}
