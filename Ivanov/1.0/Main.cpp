d6e9fb36a8f969ac2983ef8a02f218f1b019e9a2
Grigory <ivanovgrigoriinikolaevich00@gmail.com>
2018-10-10 10:13:36 +0700

    cleared code

--- Ivanov/1.0/Main.cpp
+++ Ivanov/1.0/Main.cpp
@@ -1,27 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
 
 const int MAX_LENGTH = 18;
 
-bool fillBuffer(char* buffer, int shift, int length, FILE* input) { //length without terminating null
+bool fillBuffer(char* buffer, int shift, int length, FILE* input) { ///length without terminating null
 	strcpy(buffer, buffer + shift);
-	int charcode;
 	if (fread(buffer + length - shift, sizeof(char), shift, input) != shift) {
 		buffer[length] = '\0';
 		return false;
 	}
-	/*for (int i = length - shift; i < length; ++i) {
-
-	if ((charcode = getc(input)) == EOF) {
-	buffer[i] = '\0';
-	return false;
-	}
-	else {
-	buffer[i] = charcode;
-	}
-	}*/
 	buffer[length] = '\0';
 	return true;
 }
@@ -34,7 +22,7 @@ void buildShiftTable(char* pattern, int* table, int length) {
 
 void search(char* pattern, FILE* input) {
 	int length = 0;
-	for (; pattern[length] != '\0'; ++length); // length without terminating null
+	for (; pattern[length] != '\0'; ++length); /// length without terminating null
 	char* buffer = (char*)malloc(sizeof(char)*(length + 1));
 	buffer[length] = '\0';
 	if (!fillBuffer(buffer, length, length, input)) return;
@@ -45,7 +33,6 @@ void search(char* pattern, FILE* input) {
 	int currentPosition = 1;
 	for (;;) {
 		for (int i = length - 1; i >= 0; --i) {
-			//std::cout << (currentPosition + i) << " ";
 			printf("%d ", currentPosition + i);
 			if (pattern[i] != buffer[i]) {
 				int shift = i == length - 1 ? shiftTable[buffer[i]] : shiftTable[pattern[length - 1]];
@@ -68,14 +55,21 @@ void search(char* pattern, FILE* input) {
 }
 
 int main() {
-	FILE* input = fopen("D:/Workspace/labKitchen/labs/1-0-boyer-moore/in.txt", "r");
+	FILE* input;
+	if (!(input = fopen("in.txt", "r"))){
+		printf("cannot open file\n");
+		return 1;
+	}
 	char pattern[MAX_LENGTH];
 	fgets(pattern, MAX_LENGTH, input);
-	int i;
-	for (i = 0; pattern[i] != '\n'&&pattern[i] != 0; ++i);
-	if (pattern[i] == '\n') pattern[i] = 0;
-	//gets_s(pattern);
+	for (int i = 0; pattern[i] != '\0'; ++i){
+		if (pattern[i] == '\n'){
+			pattern[i] = '\0';
+			break;
+		}
+	}
 	search(pattern, input);
 	fclose(input);
 	return 0;
+	
 }
