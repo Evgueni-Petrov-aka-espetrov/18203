a5844dbf7bf4063fa29b66a16c82c62c9e213880
ASavrova <43751079+ASavrova@users.noreply.github.com>
2018-10-10 09:52:28 +0700

    Update 3.1.cpp

--- Savrova/3.1/3.1.cpp
+++ Savrova/3.1/3.1.cpp
@@ -2,32 +2,32 @@
 #include <iostream>
 using namespace std;
 
-    void quicksort (int *arr, int first, int last)
-    {
-    	int middle, temp;
-    	int left = first, right = last;
-    	middle = arr[(left + right) / 2];
+void quicksort (int *arr, int first, int last)
+{
+	int middle, temp;
+	int left = first, right = last;
+	middle = arr[(left + right) / 2];
 
-    	while (left <= right)
-    	{
-    		while (arr[left] < middle)
-    		left ++;
-    		while (arr[right] > middle)
-    		right --;
-    		if (left <= right)
-    		{
-    			temp = arr[left];
-    			arr[left] = arr[right];
-    			arr[right] = temp;
-    			left ++;
-    			right --;
-    		}
-    	};
-    	if (first  < right)
-    	quicksort (arr, first, right);
-    	if (left < last )
-    	quicksort (arr, left,last);
-    }
+	while (left <= right)
+	{
+		while (arr[left] < middle)
+			left ++;
+		while (arr[right] > middle)
+			right --;
+		if (left <= right)
+		{
+			temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left ++;
+			right --;
+		}
+	};
+	if (first  < right)
+		quicksort (arr, first, right);
+	if (left < last )
+		quicksort (arr, left,last);
+}
 
 int main()
 {
@@ -36,10 +36,10 @@ int main()
     cin >> length;
     arr = (int*)malloc(length * sizeof(int));
     for (int index = 0; index < length; index++)
-    cin >> arr[index];
+   	 cin >> arr[index];
     quicksort (arr, 0, length - 1);
-	for (int index = 0; index < length; index++)
-    cout << arr[index]<< ' ';
+    for (int index = 0; index < length; index++)
+    	cout << arr[index]<< ' ';
 
 return 0;
 }
