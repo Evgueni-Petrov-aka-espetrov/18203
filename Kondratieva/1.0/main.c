1497469c2a04cd47e5bf04586b19791a8e990c30
Duxat <44012242+Duxat@users.noreply.github.com>
2018-10-10 11:17:55 +0300

    Update main.c

--- Kondratieva/1.0/main.c
+++ Kondratieva/1.0/main.c
@@ -5,7 +5,6 @@
 #define MAX_LEN_PAT 17
 #define MAX_LEN_TABLE 256
 
-//ñîçäàíèå òàáëèöû ñìåùåíèé
 void buildShiftTable (const unsigned char* pat, int* shiftTable){
     int len = strlen(pat);
     for (int i = 0; i < MAX_LEN_TABLE; i++)
@@ -14,13 +13,11 @@ void buildShiftTable (const unsigned char* pat, int* shiftTable){
         shiftTable[pat[i]] = len - 1 - i;
 }
 
-//ïåðåäâèíóòü shift ñèìâîëîâ ñòðîêè ñ åå êîíöà â åå íà÷àëî
 void makeShift (int shift, unsigned char* str) {
     for (int i = 0; i < shift; i++)
         str[i] = str[strlen(str) - shift + i];
 }
 
-//çàïîëíèòü áóôôåð íà÷èíàÿ ñ ñèìâîëà ñ èíäåêñîì shift
 int getString (int shift, unsigned char* str) {
     int count = fread(&str[shift], 1, (size_t) (MAX_LEN_STR - shift), stdin);
     str[shift + count] = '\0';
@@ -29,9 +26,8 @@ int getString (int shift, unsigned char* str) {
     return 0;
 }
 
-//îñóùåñòâëåíèå àëãîðèòìà áîéåðà ìóðà íà ñòðîêå
 int Search (const int* shiftTable, const unsigned char* pat, const unsigned char* str, int* n) {
-    int skip = 0, sl = strlen(str), pl = strlen(pat);//skip - äëèíà ñìåùåíèé
+    int skip = 0, sl = strlen(str), pl = strlen(pat);
     while (sl - skip >= pl) {
         int i = pl;
         do {
@@ -40,11 +36,10 @@ int Search (const int* shiftTable, const unsigned char* pat, const unsigned char
         } while(str[skip + i] == pat[i] && i > 0);
         skip += shiftTable[str[skip + pl - 1]];
     }
-    *n += skip;//äëèíà ñìåùåíèÿ ïðîøëûõ ðàáîò àëãîðèòìà
-    return sl - skip; //êîëè÷åñòâî ñèìâîëîâ âûøåäøåå çà ïðåäåëû ñòðîêè
+    *n += skip;
+    return sl - skip; 
 }
 
-//îñóùåñòâëåíèå àëãîðèòìà áîéåðà ìóðà íà òåêñòå ëþáîé äëèíû
 void boyerMoore (const int* shiftTable, unsigned char* pat, unsigned char* str) {
     int textIsOn = 1, shift = 0, n = 0;
     while (textIsOn) {
