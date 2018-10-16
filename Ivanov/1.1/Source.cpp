cbb851546d00e676b7747847ae39e8152f5ffeba
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2018-10-16 21:17:59 +0700

    i dont know what i write //upload lab1.1, problems with cyrillic letters

---
+++ Ivanov/1.1/Source.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+const int MAX_LENGTH = 18;
+
+int hashFunc(char* str) {
+	int result = 0;
+	int grade = 1;
+	for (int i = 0; str[i] != '\0'; ++i) {
+		result += (str[i] % 3)*grade;
+		grade *= 3;
+	}
+	return result;
+}
+
+int shift(char* buffer, int length, int previousHash) {
+	previousHash -= buffer[0] % 3;
+	previousHash /= 3;
+	strcpy(buffer, buffer + 1);
+	int charcode;
+	if ((charcode = getchar()) == EOF) return EOF;
+	previousHash += (charcode%3)*pow(3,length-1);
+	buffer[length - 1] = charcode;
+	buffer[length] = 0;
+	int trueHash = hashFunc(buffer);
+	return previousHash;
+}
+
+void compareStrings(char* pattern, char* buffer, int currentPosition) {
+	for (int i = 0; pattern[i] != 0; ++i) {
+		printf("%d ", currentPosition + i);
+		if (pattern[i] != buffer[i]) break;
+	}
+}
+
+void search(char* pattern) {
+	int patternHash = hashFunc(pattern);
+	printf("%d ", patternHash);
+	int length = 0;
+	for (; pattern[length] != 0; ++length);
+	char* buffer = (char*)malloc(sizeof(char)*(length + 1));
+	for (int i = 0; i < length; ++i) {
+		int charcode;
+		if ((charcode = getchar()) == EOF) {
+			free(buffer);
+			return;
+		}
+		buffer[i] = charcode;
+	}
+	buffer[length] = 0;
+
+	int currentPosition = 1;
+	int currentHash = hashFunc(buffer);
+	for (;;++currentPosition) {
+		if (currentHash == patternHash) compareStrings(pattern, buffer, currentPosition);
+		if ((currentHash = shift(buffer, length, currentHash)) == EOF) {
+			free(buffer);
+			return;
+		}
+	}
+}
+
+int main() {
+	char pattern[MAX_LENGTH];
+	gets_s(pattern);
+	search(pattern);
+	return 0;
+}
