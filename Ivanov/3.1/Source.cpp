4c855cefa893b3e4fd6605ce6b918c9fc0afa8c8
Grigory <ivanovgrigoriinikolaevich00@gmail.com>
2018-10-31 10:36:47 +0700

    upload current qsort source

---
+++ Ivanov/3.1/Source.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int* a, int* b){
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int split(int*arr, int lb, int rb){
+	int baseIndex = lb + (rb - lb) / 2;
+	int baseElement = arr[baseIndex];
+	printf("baseElement: %d\nbaseIndex: %d\n", baseElement, baseIndex);
+	int l = lb;
+	int r = rb;
+	for (;;){
+		for (;; ++l){
+			if (l == r){
+				swap(arr + baseIndex, arr + l + 1);
+				return l + 1;
+			}
+			if (arr[l] >= baseElement) break;
+		}
+		for (;; --r){
+			if (l == r){
+				swap(arr + baseIndex, arr + l);
+				return l;
+			}
+			if (arr[r] < baseElement) break;
+		}
+		if (baseIndex == l){
+			baseIndex = r;
+		}
+		else if (baseIndex == r){
+			baseIndex = l;
+		}
+		swap(arr + l, arr + r);
+	}
+}
+
+void qsort(int* arr, int lb, int rb){
+	if (rb - lb < 1) return;
+	
+}
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int)*n);
+	for (int i = 0; i < n; ++i){
+		scanf("%d", arr + i);
+	}
+
+	printf("return: %d\n", split(arr, 0, n - 1));
+
+	for (int i = 0; i < n; ++i){
+		printf("%d", arr[i]);
+	}
+
+	free(arr);
+	return 0;
+}
