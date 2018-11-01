90521f6b3aa02a5308a81c91df519a8bc90c7449
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2018-11-01 17:17:15 +0700

    qsort doesnt pass the last test

--- Ivanov/3.1/Source.cpp
+++ Ivanov/3.1/Source.cpp
@@ -1,60 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int* a, int* b){
+void swap(int* a, int* b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-int split(int*arr, int lb, int rb){
+int split(int*arr, int lb, int rb) {
 	int baseIndex = lb + (rb - lb) / 2;
 	int baseElement = arr[baseIndex];
-	printf("baseElement: %d\nbaseIndex: %d\n", baseElement, baseIndex);
 	int l = lb;
 	int r = rb;
-	for (;;){
-		for (;; ++l){
-			if (l == r){
-				swap(arr + baseIndex, arr + l + 1);
-				return l + 1;
+	for (;;) {
+		for (;; ++l) {
+			if (l == r) {
+
+				swap(arr + baseIndex, arr + l);
+				return l;
 			}
 			if (arr[l] >= baseElement) break;
 		}
-		for (;; --r){
-			if (l == r){
+		for (;; --r) {
+			if (l == r) {
 				swap(arr + baseIndex, arr + l);
 				return l;
 			}
 			if (arr[r] < baseElement) break;
 		}
-		if (baseIndex == l){
+		if (baseIndex == l) {
 			baseIndex = r;
 		}
-		else if (baseIndex == r){
+		else if (baseIndex == r) {
 			baseIndex = l;
 		}
 		swap(arr + l, arr + r);
 	}
 }
 
-void qsort(int* arr, int lb, int rb){
+void qsort(int* arr, int lb, int rb) {
 	if (rb - lb < 1) return;
-	
+	int baseIndex = split(arr, lb, rb);
+	qsort(arr, lb, baseIndex - 1);
+	qsort(arr, baseIndex + 1, rb);
 }
 
-int main(){
+int main() {
 	int n;
 	scanf("%d", &n);
 	int* arr = (int*)malloc(sizeof(int)*n);
-	for (int i = 0; i < n; ++i){
+	for (int i = 0; i < n; ++i) {
 		scanf("%d", arr + i);
 	}
 
-	printf("return: %d\n", split(arr, 0, n - 1));
+	qsort(arr, 0, n - 1);
 
-	for (int i = 0; i < n; ++i){
-		printf("%d", arr[i]);
+	for (int i = 0; i < n; ++i) {
+		printf("%d ", arr[i]);
 	}
 
 	free(arr);
