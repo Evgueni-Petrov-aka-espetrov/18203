e0c7a8cfa18f7abb59fa4206fae00f5506c3021f
FatCat343 <34466386+FatCat343@users.noreply.github.com>
2018-11-06 21:19:16 +0600

    Add files via upload

--- Shishkin/1.0/BM.c
+++ Shishkin/1.0/BM.c
@@ -1,65 +1,95 @@
 #include <stdio.h>
 #include <string.h>
-#include <malloc.h>
-int booyer (char haystack[], char needle[], FILE *fout, int lengthneedle, int lengthhay) {
-	int i, j, l, var, ascichar1, count;
-	int table[256];
-	//printf("%s ", needle);
-	//lengthhay = strlen(haystack);
-	//lengthneedle = strlen(needle);
-//	printf("%d ", lengthhay);
-	if ((lengthhay >= lengthneedle) && (lengthneedle > 0) && (lengthhay > 2)) {
-		for (i=0; i<256; i++) 
-			table[i] = lengthneedle;
-			
-		for(i=lengthneedle-2; i>=0; i--){
-			ascichar1 = needle[i];
-			if (table[ascichar1] == lengthneedle) table[ascichar1] = lengthneedle-1-i;
-			//printf("%d ", needle[i]);
-			//printf("%d ", table[needle[i]]);
+
+int firstread(unsigned char hay[], int lengthneedle, FILE *fin) {
+	int i;
+	int pos = 0;
+	char symbol;
+	int errornum = 0;
+	for (i = 0; i < lengthneedle; i++){
+		symbol = getc(fin);
+		if (symbol == EOF) {
+			i = lengthneedle + 1;
+			errornum++;
 		}
-		//table[needle[lengthneedle-1]] = lengthneedle;
-		//printf("%d ", table[needle[8]]);
-		j = lengthneedle - 1;
-		i = j;
-		l = j;
-		while (j <= lengthhay-1) {
-			//printf("111111");
-			count = 0;
-			do {
-				fprintf(fout, "%d ", j+1);
-				//printf("%d ", j+1);
-				i--;
-				j--;
-				count++;
-				
-			} while ((i >= 0) && (haystack[j+1] == needle[i+1]));
-			
-			if (i < 0) var =1;
-			if (haystack[j+1] != needle[i+1]) var = 2;
-			
-			if (var==1) {
-				i = lengthneedle - 1;
-				j = l + lengthneedle;
-				l = j;
-				//printf(" 11 ");
-			}
-			if (var==2) {
-				
-				i = lengthneedle - 1;
-				if (count == 1) j = l + table[haystack[j+1]];
-				if (count > 1) j = l + table[needle[i]];
-				l = j;
-			} 
-			//printf("%d ", j);
+		else {
+			hay[pos] = symbol;
+			pos++;
 		}
 	}
-	//else fprintf(fout, "bad input");
+	if (errornum > 0) return 1;
+	else return 0;
 }
+
+int nextread(unsigned char hay[], int lengthneedle, int move, FILE *fin) {
+	char symbol;
+	int errornum = 0;
+	int i;
+	for (i = move; i < lengthneedle; i++) {
+		hay[i - move] = hay[i];
+	}
+	printf("%d  ", move);
+	for (i=lengthneedle-move; i < lengthneedle; i++) {
+		symbol = getc(fin);
+		if (symbol == EOF) {
+			i = lengthneedle + 1;
+			errornum++;
+		}
+		else {
+			hay[i] = symbol;
+		}
+	}
+	if (errornum > 0) return 1;
+	else return 0;
+}
+void booyer (unsigned char haystack[], unsigned char needle[], FILE *fout, FILE *fin, int lengthneedle) {
+	int j, l, var, ascichar1;
+	int table[256];
+	int i;
+	int move;
+	for (i=0; i<256; i++) 
+		table[i] = lengthneedle;
+		
+	for(i=lengthneedle-2; i>=0; i--){
+		ascichar1 = needle[i];
+		if (table[ascichar1] == lengthneedle) table[ascichar1] = lengthneedle-1-i;
+	}
+	
+	int result = firstread(haystack, lengthneedle, fin);
+	//printf("%d", result);
+	int start = 1;
+	int count = 0;
+	while (result != 1) {
+		int i;
+		for (i=lengthneedle-1; i>=0; i--) {
+			fprintf(fout, "%d ", i+start);
+			count++;
+			if (haystack[i] != needle[i]) break;
+			
+		}
+		//printf("%d", result);
+		//finished for
+		if (i<0) {
+			move = lengthneedle;
+			printf("f", result);
+		} 
+		//exited for
+		if (i>=0) {
+			if (count == 1) move = table[haystack[i+1]];
+			if (count > 1) move = table[needle[i]];	
+			printf("g  ", count);
+		}
+		result = nextread(haystack, lengthneedle, move, fin);
+		start = start + move;
+	}
+	
+}
+
+
 int main () {
 	FILE *fin, *fout;
-	char *haystack;
-	char needle[16];
+	unsigned char haystack[16];
+	unsigned char needle[16];
 	char f, symbol;
 	int c, d, i, lengthhay, lengthneedle;
 	fout = fopen("out.txt", "w");
@@ -68,31 +98,17 @@ int main () {
 	if (fin == NULL) 
 		fprintf(fout, "File could not be opened.");
 	else {
-		haystack = (char*)malloc(sizeof(char));
+		
 		i = 0;
 		while ((symbol=getc(fin)) != '\n'){
 			needle[i] = symbol;
  			i++;
-				
 		}
 		lengthneedle = i;
-		//printf("%s  ", needle);
-		//printf("  %d   ", strlen(needle));
-		c = 0;
-		while ((f=getc(fin)) != EOF) {
-			haystack[c] = f;
-			c++;
-			realloc(haystack, (c+1)*sizeof(char));	
-		}
-		lengthhay = c;
-		
-		//printf("%s  ", haystack);
-		//printf("  %d   ", strlen(haystack));
-		
-	
-		booyer(haystack, needle, fout, lengthneedle, lengthhay);
 
-		//fgets(f, 1, fout);
+		booyer(haystack, needle, fout, fin, lengthneedle);
+
+
 		
 	}
 	fclose(fin);
