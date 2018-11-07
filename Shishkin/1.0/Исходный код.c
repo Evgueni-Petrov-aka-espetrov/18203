0562fd0b372465bf4e6f68d1d4f1d3ebbc8b8358
FatCat343 <34466386+FatCat343@users.noreply.github.com>
2018-11-07 10:13:17 +0700

    Add files via upload

--- Shishkin/1.0/Исходный код.c
+++ Shishkin/1.0/Исходный код.c
@@ -28,7 +28,6 @@ int nextread(unsigned char hay[], int lengthneedle, int move, FILE *fin) {
 	for (i = move; i < lengthneedle; i++) {
 		hay[i - move] = hay[i];
 	}
-	//printf("%d --- ", move);
 	for (i = lengthneedle - move; i < lengthneedle; i++) {
 		symbol = getc(fin);
 		if (symbol == EOF) {
@@ -43,22 +42,18 @@ int nextread(unsigned char hay[], int lengthneedle, int move, FILE *fin) {
 	else return 0;
 }
 void booyer(unsigned char haystack[], unsigned char needle[], FILE *fout, FILE *fin, int lengthneedle) {
-	int j, l, var, ascichar1;
+	int ascichar1;
 	int table[256];
 	int i;
 	int move;
 	for (i = 0; i<256; i++)
 		table[i] = lengthneedle;
-
 	for (i = lengthneedle - 2; i >= 0; i--){
 		ascichar1 = needle[i];
 		if (table[ascichar1] == lengthneedle) table[ascichar1] = lengthneedle - 1 - i;
 	}
-
 	int result = firstread(haystack, lengthneedle, fin);
-	//printf("%d", result);
 	int start = 1;
-	
 	while (result != 1) {
 		int i;
 		int count = 0;
@@ -66,51 +61,38 @@ void booyer(unsigned char haystack[], unsigned char needle[], FILE *fout, FILE *
 			fprintf(fout, "%d ", i + start);
 			count++;
 			if (haystack[i] != needle[i]) break;
-
 		}
-		//printf("%d", result);
 		//finished for
 		if (i<0) {
 			move = lengthneedle;
-			//printf("%d -", result);
 		}
 		//exited for
 		if (i >= 0) {
 			if (count == 1) move = table[haystack[i]];
 			if (count > 1) move = table[needle[lengthneedle-1]];
-			//printf("%d  ", count);
 		}
 		result = nextread(haystack, lengthneedle, move, fin);
 		start = start + move;
 	}
-
 }
 
-
 int main() {
-	FILE *fin, *fout;
-	unsigned char haystack[16];
-	unsigned char needle[16];
-	char f, symbol;
-	int c, d, i, lengthhay, lengthneedle;
-	fout = fopen("out.txt", "w");
-	fin = fopen("in.txt", "r");
-
+	FILE *fout = fopen("out.txt", "w");
+	FILE *fin = fopen("in.txt", "r");
 	if (fin == NULL)
 		fprintf(fout, "File could not be opened.");
 	else {
-
-		i = 0;
+		unsigned char haystack[16];
+		unsigned char needle[16];
+		char symbol;
+		int lengthneedle;
+		int i = 0;
 		while ((symbol = getc(fin)) != '\n'){
 			needle[i] = symbol;
 			i++;
 		}
 		lengthneedle = i;
-
 		booyer(haystack, needle, fout, fin, lengthneedle);
-
-
-
 	}
 	fclose(fin);
 	fclose(fout);
