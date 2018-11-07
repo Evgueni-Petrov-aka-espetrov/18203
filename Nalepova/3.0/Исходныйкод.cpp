5a6dcac07da34cc8c8688e6de8fc0b908b558361
aeradcs <43800886+aeradcs@users.noreply.github.com>
2018-11-07 10:07:05 +0700

    Add files via upload

---
+++ Nalepova/3.0/Исходныйкод.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+void Sort(int *a, int begin, int end)
+{
+	int temp;
+	int left = begin, right = end;
+	int pivot = a[(right + left) / 2];
+
+
+	while (left < right)
+	{
+		while (a[right] > pivot)
+			right--;
+		while (a[left] < pivot)
+			left++;
+		if (left <= right)
+		{
+			temp = a[left];
+			a[left] = a[right];
+			a[right] = temp;
+			left++;
+			right--;
+		}
+	}
+	if (begin < right)
+		Sort(a, begin, right);
+	if (left < end)
+		Sort(a, left, end);
+}
+void main()
+{
+	int *a;
+	int n;
+	scanf("%d", &n);
+	a = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	Sort(a, 0, n - 1);
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	free(a);
+}
