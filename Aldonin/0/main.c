8b3e01fe5fb8011df50b4f1494be504ae82dd908
Florane <qwerty.qa912@gmail.com>
2018-11-11 11:39:31 +0300

    Bug fixes

--- Aldonin/0/main.c
+++ Aldonin/0/main.c
@@ -1,30 +1,15 @@
-#include "stdio.h"
-#include "translate.h"
+#include <stdio.h>
+#include "boyer_moore.h"
 
 int main()
 {
-/*-----------------------------------------------*/
-    int b1, b2;
-    char enter[255];
-    FILE *input = fopen("in.txt","r");
-
-    fscanf(input,"%d %d",&b1,&b2);
-    fscanf(input,"%s",enter);
-/*-----------------------------------------------*/
-    int test = failSafe(b1, b2, enter);
-    if(test != 0)
-    {
-        printf("bad input",test);
-        return 0;
-    }
-/*-----------------------------------------------*/
-    double d = numToDouble(b1, enter);
-    char c[255];
-
-    doubleToNum(c, b2, d);
-    
+    FILE *input = fopen("in.txt", "r");
     FILE *output = fopen("out.txt","w");
-    fprintf(output,"%s",c);
-/*-----------------------------------------------*/
+
+    search(input, output);
+
+    fclose(input);
+    fclose(output);
+
     return 0;
 }
