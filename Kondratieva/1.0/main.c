89190cb2d2b94a5a0389f8b6ebc08c563c89a83b
Duxat <44012242+Duxat@users.noreply.github.com>
2018-11-28 11:35:51 +0700

    Add files via upload

--- Kondratieva/1.0/main.c
+++ Kondratieva/1.0/main.c
@@ -6,7 +6,7 @@
 #define MAX_LEN_TABLE 256
 
 void buildShiftTable(const unsigned char* pat, int* shiftTable) {
-	int len = strlen((const char*)pat);
+	size_t len = strlen((const char*)pat);
 	for (int i = 0; i < MAX_LEN_TABLE; i++)
 		shiftTable[i] = len;
 	for (int i = 0; i < len - 1; i++)
@@ -19,33 +19,33 @@ void makeShift(int shift, unsigned char* str) {
 }
 
 int getString(int shift, unsigned char* str) {
-	int count = fread(&str[shift], 1, (size_t)(MAX_LEN_STR - shift), stdin);
+	size_t count = fread(&str[shift], 1, (size_t)(MAX_LEN_STR - shift - 1), stdin);
 	str[shift + count] = '\0';
-	if (count == MAX_LEN_STR - shift)
+	if (count == MAX_LEN_STR - shift - 1)
 		return 1;
 	return 0;
 }
 
-int Search(const int* shiftTable, const unsigned char* pat, const unsigned char* str) {
-	int skip = 0, strLength = strlen((const char*)str), patLength = strlen((const char*)pat);
-	static int count = 0;
+int search(const int* shiftTable, const unsigned char* pat, const unsigned char* str, int* count) {
+	int skip = 0;
+	size_t strLength = strlen((const char*)str), patLength = strlen((const char*)pat);
 	while (strLength - skip >= patLength) {
 		int i = patLength;
 		do {
-			printf("%d ", count + skip + i);
+			printf("%d ", *count + skip + i);
 			i--;
 		} while (str[skip + i] == pat[i] && i > 0);
 		skip += shiftTable[str[skip + patLength - 1]];
 	}
-	count += skip;
+	*count += skip;
 	return strLength - skip;
 }
 
 void boyerMoore(const int* shiftTable, unsigned char* pat, unsigned char* str) {
-	int textIsOn = 1, shift = 0;
+	int textIsOn = 1, shift = 0, count = 0;
 	while (textIsOn) {
 		textIsOn = getString(shift, str);
-		shift = Search(shiftTable, pat, str);
+		shift = search(shiftTable, pat, str, &count);
 		makeShift(shift, str);
 	}
 }
