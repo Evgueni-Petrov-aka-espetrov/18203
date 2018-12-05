e1e34e678d8882cbd62b182b597ed3db4c471f95
Duxat <44012242+Duxat@users.noreply.github.com>
2018-12-05 08:09:16 +0700

    Add files via upload

--- Kondratieva/3.1/qsort.c
+++ Kondratieva/3.1/qsort.c
@@ -1,55 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
 
-int* getElements(int* arr, int arrSize) {
-    arr = (int*) malloc(sizeof(int)*arrSize);
-    for (int i = 0; i < arrSize; i++) {
-        scanf("%d", &arr[i]);
-    }
-    return arr;
-}
 
-void printAnswer(int* arr, int arrSize) {
-    for (int i = 0; i < arrSize; i++) {
-        printf("%d ", arr[i]);
-    }
-}
-
-int partition(int* arr, int left, int right) {
-    int pivot = arr[(left + right)/2];
-    while (left <= right) {
-        while(arr[left] < pivot) left++;
-        while(arr[right] > pivot) right--;
-        if (left <= right) {
-            swap(&arr[left], &arr[right]);
-            left++;
-            right--;
-        }
-    }
-    return left;
-}
-
-void quickSort(int* arr, int left, int right) {
-    int delimiter = partition(arr, left, right);
-    if (delimiter < right)
-        quickSort(arr, delimiter, right);
-    if (delimiter - 1 > left)
-        quickSort(arr, left, delimiter - 1);
+void quickSort(int* arr, int first, int last) {
+	int pivot = 0, left = first, right = last;
+	pivot = arr[(left + right) / 2];
+	while (left < right) {
+		while (arr[left] < pivot) left++;
+		while (arr[right] > pivot) right--;
+		if (left <= right) {
+			swap(&arr[left], &arr[right]);
+			left++;
+			right--;
+		}
+	}
+	if (first < right) quickSort(arr, first, right);
+	if (left < last) quickSort(arr, left, last);
 }
 
 int main() {
-    int arrSize;
-    int* arr;
-    scanf("%d", &arrSize);
-    arr = getElements(arr, arrSize);
-    quickSort(arr, 0, arrSize - 1);
-    printAnswer(arr, arrSize);
-    free(arr);
-    return 0;
+	int arrSize;
+	scanf("%d", &arrSize);
+	int* arr = (int*)malloc(sizeof(int)*arrSize);
+	for (int i = 0; i < arrSize; i++) {
+		scanf("%d", &arr[i]);
+	}
+	quickSort(arr, 0, arrSize - 1);
+	for (int i = 0; i < arrSize; i++) {
+		printf("%d ", arr[i]);
+	}
+	free(arr);
+	return 0;
 }
