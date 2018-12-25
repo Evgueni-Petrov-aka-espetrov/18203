1426135459b0826187eea6843c8f09dbe8c1e3d0
ASavrova <43751079+ASavrova@users.noreply.github.com>
2018-12-25 23:34:09 +0700

    Update 1.0.cpp

--- Savrova/1.0/1.0.cpp
+++ Savrova/1.0/1.0.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#define MAX_LEN_STR 16
-#define MAX_LEN_PAT 16
-#define MAX_LEN_TABLE 256
-
-void buildShiftTable(unsigned char* pat, int* shiftTable);
-void BM(int* shiftTable, unsigned char* pat, unsigned char* str);
-int search(const int* shiftTable,unsigned char* pat, unsigned char* str, int* count);
-void makeShift(int shift, unsigned char* str);
 int getstr(int shift, unsigned char* str);
-using namespace std;
+void makeShift(int shift, unsigned char* str);
+int search(int* shiftTable,unsigned char* pat, unsigned char* str, int* count);
+void buildShiftTable( unsigned char* pat, int* shiftTable);
+void BM( int* shiftTable, unsigned char* pat, unsigned char* str);
 
-int main() {
-	unsigned char str[MAX_LEN_STR] = { 0 };
-	unsigned char pat[MAX_LEN_PAT];
-	int shiftTable[MAX_LEN_TABLE];
-	cin >> pat;
+int main()
+{
+	unsigned char str[17] = {0};
+	unsigned char pat[17];
+	int shiftTable[256];
+	scanf("%[^\n]s", pat);
 	getchar();
 	buildShiftTable(pat, shiftTable);
 	BM(shiftTable, pat, str);
@@ -24,36 +20,36 @@ int main() {
 void buildShiftTable(unsigned char* pat, int* shiftTable)
 {
 	int pat_length = strlen((char*)pat);
-	for (int i = 0; i < MAX_LEN_TABLE; i++)
+	for (int i = 0; i < 256; i++)
 		shiftTable[i] = pat_length;
 	for (int i = 0; i < pat_length - 1; i++)
 		shiftTable[pat[i]] = pat_length - 1 - i;
 }
 
 void BM(int* shiftTable, unsigned char* pat, unsigned char* str)
-{
+{   int y = 1;
 	int  shift(0);
 	int count(0);
-	while (getstr(shift, str)!=0)
-	{
-		shift = search(shiftTable, pat, str, &count);
+	while (y)
+	{   y = getstr(shift, str);
+	    shift = search(shiftTable, pat, str, &count);
 		makeShift(shift, str);
 	}
 }
 
-int search(const int* shiftTable,unsigned char* pat, unsigned char* str, int* count)
+int search(int* shiftTable, unsigned char* pat, unsigned char* str, int* count)
 {
 	int skip = 0;
-	int str_length = strlen(( char*)str);
+	int str_length = strlen((char*)str);
 	int pat_length = strlen((char*)pat);
 	while (str_length - skip >= pat_length)
 	{
 		int i = pat_length;
-		while (str[skip + i] == pat[i] && i > 0)
+		do
 		{
-			cout << *count + skip + i <<" ";
+			printf("%d ", *count + skip + i);
 			i--;
-		}
+		} while (str[skip + i] == pat[i] && i > 0);
 		skip += shiftTable[str[skip + pat_length - 1]];
 	}
 	*count += skip;
@@ -63,20 +59,14 @@ int search(const int* shiftTable,unsigned char* pat, unsigned char* str, int* co
 void makeShift(int shift, unsigned char* str)
 {
 	for (int i = 0; i < shift; i++)
-		str[i] = str[strlen((char*)str) - shift + i];
+		str[i] = str[strlen(( char*)str) - shift + i];
 }
 
 int getstr(int shift, unsigned char* str)
 {
-	int count = fread(&str[shift], 1, MAX_LEN_STR - shift, stdin);
+	int count = fread(&str[shift], 1, 16 - shift, stdin);
 	str[shift + count] = '\0';
-	if (count == MAX_LEN_STR - shift)
+	if (count == 16 - shift)
 		return 1;
 	return 0;
 }
-
-
-
-
-
-
