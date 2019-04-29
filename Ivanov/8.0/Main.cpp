e91b09b5da2ab64c2c72d451b625fac4c3e91b60
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2019-04-29 14:17:19 +0700

    cruscal beta

--- Ivanov/8.0/Main.cpp
+++ Ivanov/8.0/Main.cpp
@@ -4,7 +4,7 @@
 #include "heapsort.h"
 
 int findRootIndex(short* parents, int vertexIndex) {
-	return parents[vertexIndex] == 0 ? vertexIndex : findRootIndex(parents, parents[vertexIndex]);
+	return parents[vertexIndex] == vertexIndex ? vertexIndex : findRootIndex(parents, parents[vertexIndex]);
 }
 
 void mergeTrees(short* parents, short* heights, int root1, int root2) {
@@ -22,7 +22,6 @@ void mergeTrees(short* parents, short* heights, int root1, int root2) {
 }
 
 int main(){
-	return 0;
 	int N, M;
 	if (scanf("%d %d", &N, &M) != 2) {
 		printf("bad number of lines");
@@ -33,35 +32,60 @@ int main(){
 		return 0;
 	}
 	if (!(0 <= M && M <= (N*(N - 1) / 2))) {
-		printf("bad number of vertices");
+		printf("bad number of edges");
+		return 0;
+	}
+	if (M == 0) {
+		printf("no spanning tree");
 		return 0;
 	}
 	TEdge* edgeStorage = (TEdge*)malloc(sizeof(TEdge)*M);
 	for (int i = 0; i < M; ++i) {
-		int v1, v2, length;
-		if (scanf("%d %d %d", &v1, &v2, &length) != 3) {
+		int v1, v2;
+		long long length;
+		if (scanf("%d %d %lld", &v1, &v2, &length) != 3) {
 			printf("bad number of lines");
-			return 1;
+			return 0;
 		}
 		if (!(1 <= v1 && v1 <= N) || !(1 <= v2 && v2 <= N)) {
 			printf("bad vertex");
-			return 1;
+			return 0;
 		}
-		if (!(0 <= length && length <= INT_MAX)) {////////////???
+		if (!(0 <= length && length <= INT_MAX)) {
 			printf("bad length");
-			return 1;
+			return 0;
 		}
 		edgeStorage[i].v1 = (short)v1;
 		edgeStorage[i].v2 = (short)v2;
-		edgeStorage[i].length = length;
+		edgeStorage[i].length = (int)length;
 	}
 	heapsort(edgeStorage, M);
 	short* parents = (short*)malloc(sizeof(short)*N);
-	char* heights = (char*)malloc(sizeof(char)*N);
+	short* heights = (short*)malloc(sizeof(short)*N);
 	for (int i = 0; i < N; ++i) {
-		parents[i] = heights[i] = 0;
+		parents[i] = i;
+		heights[i] = 0;
 	}
+	TEdge* resultStorage = (TEdge*)malloc(sizeof(TEdge)*M);
+	int storageSize = 0;
 	for (int i = 0; i < M; ++i) {
-
+		if (findRootIndex(parents, edgeStorage[i].v1-1) != findRootIndex(parents, edgeStorage[i].v2-1)) {
+			resultStorage[storageSize++] = edgeStorage[i];
+			mergeTrees(parents, heights, edgeStorage[i].v1-1, edgeStorage[i].v2-1);
+		}
 	}
+	for (int i = 1; i < N; ++i){
+		if (findRootIndex(parents, i) != findRootIndex(parents, 0)){
+			printf("no spanning tree");
+			return 0;
+		}
+	}
+
+	for (int i = 0; i < storageSize; ++i){
+		printf("%d %d\n", resultStorage[i].v1, resultStorage[i].v2);
+	}
+
+
+
+	return 0;
 }
